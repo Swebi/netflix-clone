c8b0978bb86145b69c64418fbc3bd3bdf5455b51
swebi <iamsuhaybahmed@gmail.com>
2023-09-03 07:18:17 +0000

    fixes

--- script.js
+++ script.js
@@ -3,23 +3,14 @@
 
 function redfunction()
 {
-    
-    email = document.getElementById('email').value
-    password = document.getElementById('pwd').value
 
-
-    if (email.length == 0 && password.length <= 4)
-    {
         document.getElementById("emailtext").innerHTML = 'Please enter a valid email address or phone number.';
         document.getElementById("pwdtext").innerHTML = 'Your password must contain between 4 and 60 characters.';
 
         document.getElementById("email").style.borderColor = 'black black #e87c03 black';
         document.getElementById("pwd").style.borderColor = 'black black #e87c03 black';
-    }
-    else
-    {
-        window.location.href='login.html';
 
-    }
 
+
+}
 
