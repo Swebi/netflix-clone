8c1d11a46fcc3309557a59af67d2d71d05d9a71d
swebi <iamsuhaybahmed@gmail.com>
2023-09-03 00:50:40 +0000

    added js component

---
+++ script.js
@@ -0,0 +1,15 @@
+
+function redfunction()
+{
+
+        
+        document.getElementById("emailtext").innerHTML = 'Please enter a valid email address or phone number.';
+        document.getElementById("pwdtext").innerHTML = 'Your password must contain between 4 and 60 characters.';
+        
+        document.getElementById("email").style.borderColor = 'black black #e87c03 black';
+        document.getElementById("pwd").style.borderColor = 'black black #e87c03 black';
+
+
+
+}
+
