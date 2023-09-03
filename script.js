42cb9ccedd6fc1dc8eeeab7edfb4477a4f6e3386
swebi <iamsuhaybahmed@gmail.com>
2023-09-03 07:05:17 +0000

    fixes

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 
+
+
 function redfunction()
 {
+    
+    email = document.getElementById('email').value
+    password = document.getElementById('pwd').value
 
-        
+
+    if (email.length == 0 && password.length <= 4)
+    {
         document.getElementById("emailtext").innerHTML = 'Please enter a valid email address or phone number.';
         document.getElementById("pwdtext").innerHTML = 'Your password must contain between 4 and 60 characters.';
 
         document.getElementById("email").style.borderColor = 'black black #e87c03 black';
         document.getElementById("pwd").style.borderColor = 'black black #e87c03 black';
-        /* Can't use border-color like CSS, so the alternate is borderColor*/
+    }
+    else
+    {
+        window.location.href='login.html';
 
+    }
 
-
-}
 
